0ad164d8fb5f54bda6772d1ba7ebe8d7e95a0917
Arun Kumar <frostarun@gmail.com>
2018-06-17 02:22:45 +0530

    Ui Changes with Manager Impl

---
+++ assets/js/manager-tab.js
@@ -0,0 +1,29 @@
+// var myApp = angular.module('dockerUtility', []);
+// myApp.controller('mainCtrl', function ($scope) {
+//     $scope.handleOption = function () {
+//         $scope.optionTable = true;
+//     };
+// });
+
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("manager-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("manager-tablinks");
+    // tablinks[0].className = tablinks[0].className.replace("manager-tablinks", " active");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
--- docker_controller.js
+++ docker_controller.js
@@ -5,7 +5,7 @@
      $scope.scanDockerHost = function () {
          var hostip = angular.element(document.getElementById("hostIP"));
          $scope.dockerhost = hostip.val();
-        //  $scope.dockerhost = "127.0.0.1:2376";
+         $scope.dockerhost = "127.0.0.1:2376";
          $scope.start();
          var docker_container_url = "http://" + $scope.dockerhost + "/containers/json?all=1";
          var docker_image_url = "http://" + $scope.dockerhost + "/images/json?all=1";
@@ -16,7 +16,6 @@
          var config = {
              headers: {
                  'Access-Control-Allow-Origin': '*'
-                 // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
              }
          };
 
@@ -41,7 +40,64 @@
 
      };
 
+     $scope.inspectDockerContainer = function () {
+         var containerID = angular.element(document.getElementById("containerID"));
+         $scope.containerID = containerID.val();
 
+         var hostip = angular.element(document.getElementById("inspectHostIP"));
+         $scope.dockerhost = hostip.val();
+
+         $scope.dockerhost = "127.0.0.1:2376";
+         $scope.start();
+         var docker_container_url = "http://" + $scope.dockerhost + "/containers/"+$scope.containerID+"/json";
+
+         console.log($scope.containerID);
+         console.log($scope.dockerhost);
+
+         var config = {
+             headers: {
+                 'Access-Control-Allow-Origin': '*'
+             }
+         };
+
+         $http.get(docker_container_url, config).
+         then(function (response) {
+             $scope.inspectContainerTable = true;
+             $scope.inspectContainer = response.data;
+         });
+     };
+
+
+     $scope.deleteDockerContainer = function () {
+         var containerID = angular.element(document.getElementById("containerID"));
+         $scope.containerID = containerID.val();
+
+         var hostip = angular.element(document.getElementById("hostIP"));
+         $scope.dockerhost = hostip.val();
+
+         $scope.dockerhost = "127.0.0.1:2376";
+         $scope.start();
+
+         var docker_container_url = "http://" + $scope.dockerhost + "/containers/"+$scope.containerID+"/json";
+
+         console.log($scope.containerID);
+
+         var config = {
+             headers: {
+                 'Access-Control-Allow-Origin': '*'
+             }
+         };
+
+         $http.get(docker_container_url, config).
+         then(function (response) {
+             $scope.statisticsTable = true;
+             $scope.containers = response.data;
+         });
+     };
+
+     $scope.handleOption = function () {
+        $scope.optionTable = true;
+    };
 
      $scope.start = function () {
          cfpLoadingBar.start();
