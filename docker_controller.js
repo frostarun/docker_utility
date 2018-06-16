d4d8722e84d5cd4339f5f293dc340552a76a6110
Arun Kumar <frostarun@gmail.com>
2018-06-16 22:18:05 +0530

    report enhancements

--- docker_controller.js
+++ docker_controller.js
@@ -1,44 +1,53 @@
  var myApp = angular.module('myApp', ['angular-loading-bar']);
- myApp
-     //  .config(function (cfpLoadingBarProvider) {
-     //      cfpLoadingBarProvider.includeSpinner = true;
-     //  })
-     .controller('myCtrl', function ($scope, $http,cfpLoadingBar) {
-         $scope.dockerhost = "127.0.0.1:2376";
-
-         $scope.scanDockerHost = function () {
-             
-             $scope.start();
-             var dockerurl = "http://" + $scope.dockerhost + "/containers/json?all=1";
-             var imageurl = "http://" + $scope.dockerhost + "/images/json?all=1";
+ myApp.controller('myCtrl', function ($scope, $http, cfpLoadingBar) {
 
 
-             console.log(dockerurl);
+     $scope.scanDockerHost = function () {
+         var hostip = angular.element(document.getElementById("hostIP"));
+         $scope.dockerhost = hostip.val();
+        //  $scope.dockerhost = "127.0.0.1:2376";
+         $scope.start();
+         var docker_container_url = "http://" + $scope.dockerhost + "/containers/json?all=1";
+         var docker_image_url = "http://" + $scope.dockerhost + "/images/json?all=1";
+         var docker_info_url = "http://" + $scope.dockerhost + "/info";
 
-             $http.get(dockerurl).
-             then(function (response) {
-                //  $scope.complete();
-                 $scope.statisticsTable = true;
-                 $scope.containers = response.data;
-             });
+         console.log($scope.dockerhost);
 
-             $http.get(imageurl).
-             then(function (response) {
-                 $scope.complete();
-                 $scope.statisticsTable = true;
-                 $scope.images = response.data;
-             });
-
-
+         var config = {
+             headers: {
+                 'Access-Control-Allow-Origin': '*'
+                 // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+             }
          };
 
-         
+         $http.get(docker_container_url, config).
+         then(function (response) {
+             $scope.statisticsTable = true;
+             $scope.containers = response.data;
+         });
 
-         $scope.start = function () {
-             cfpLoadingBar.start();
-         };
+         $http.get(docker_image_url, config).
+         then(function (response) {
+             $scope.statisticsTable = true;
+             $scope.images = response.data;
+         });
 
-         $scope.complete = function () {
-             cfpLoadingBar.complete();
-         }
-     });
+         $http.get(docker_info_url, config).
+         then(function (response) {
+             $scope.complete();
+             $scope.statisticsTable = true;
+             $scope.infos = response.data;
+         });
+
+     };
+
+
+
+     $scope.start = function () {
+         cfpLoadingBar.start();
+     };
+
+     $scope.complete = function () {
+         cfpLoadingBar.complete();
+     };
+ });
